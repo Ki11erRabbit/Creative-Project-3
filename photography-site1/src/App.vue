e9651a68f69524e9fbc88c8eacf5dada8fb862f5
ki11errabbit <unlikelytitan@gmail.com>
2021-11-03 16:56:02 -0600

    Added Some text to main, changed about to contact.

--- photography-site1/src/App.vue
+++ photography-site1/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/contact">Contact</router-link>
     </div>
     <router-view/>
   </div>
@@ -14,19 +14,20 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
   padding: 30px;
+  background-color: #fff9ec; /* warm tan-white color */
+  border: 2px solid #3c4253; /* a nice dark bluish grey */
+  border-radius: 5px;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #42b983; /*TODO change to different color*/
 }
 </style>
--- photography-site1/src/views/Home.vue
+++ photography-site1/src/views/Home.vue
@@ -1,18 +1,84 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
+
+      <div class="page">
+      <div class="content">
+
+          <!--<img class="photo" src="images/photos/JCS.jpg" alt="Japanese-Colonel-Sanders">    to be added back in later-->
+
+        <p>In need of a third or fith hand for all of those precious moments in
+        life? Then allow me to be your photographer. I'll be there for your marriage.
+      I'll be there for your first baby photos. I'll be there for your child's first day
+    of school. Then when they are off to college I'll be there too. And when you are
+    celebrating your 50th anniversary, I'll hopefully be there for you too.</p>
+      </div>
+    <!-- rick astley quote -->
+
+    </div>
+
+    <div class="footer">
+
+    <a href="https://github.com/Ki11erRabbit/Creative-Project-3"> GitHub
+    <p>Made by Alec Davis and Louisa Dayton</p></a>
+    </div>
   </div>
 </template>
 
+
+
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
+/*export default {
   name: 'Home',
   components: {
     HelloWorld
   }
-}
+}*/
 </script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  color: #3c4253; /* a nice dark bluish grey */
+  box-sizing: border-box;
+}
+body {
+  background-color: #d8dce5; /* blueish light grey */
+}
+
+.page {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  background-color: #d8dce5; /* blueish light grey */
+}
+
+.content {
+  background-color: #fff9ec; /* warm tan-white color */
+  max-width: 900px;
+  margin-left: auto;
+  margin-right: auto;
+  border: 2px solid #3c4253; /* a nice dark bluish grey */
+  border-radius: 5px;
+}
+
+.content {
+  padding: 100px;
+}
+.footer {
+  background-color: #fff9ec; /* warm tan-white color */
+  border: 2px solid #3c4253 ; /* a nice dark bluish grey */
+  border-radius: 5px;
+  box-sizing: border-box;
+  text-align: center;
+  padding-top: 2%;
+  padding-bottom: 2%;
+}
+
+
+</style>
